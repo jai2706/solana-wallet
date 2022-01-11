7bcfc7efa8ea3743665379edba6aa0ccda3952b3
Jai Vaidya <jaivaidya27@gmail.com>
2022-01-11 08:27:32 +0530

    initial commit

---
+++ components/TransactionLayout/index.tsx
@@ -0,0 +1,165 @@
+// Import any additional classes and/or functions needed from Solana's web3.js library as you go along:
+import React, { useState, ReactElement } from "react";
+import { message } from "antd";
+import { useGlobalState } from "../../context";
+import {
+  LAMPORTS_PER_SOL,
+  Connection,
+  clusterApiUrl,
+  SystemProgram,
+  PublicKey,
+  Transaction,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+const converter = require("number-to-words");
+import { LoadingOutlined } from "@ant-design/icons";
+import { refreshBalance } from "../../utils";
+import {
+  CheckContainer,
+  CheckImage,
+  CheckFrom,
+  Processed,
+  CheckDate,
+  RecipientInput,
+  AmountInput,
+  SignatureInput,
+  AmountText,
+  RatioText,
+} from "../../styles/StyledComponents.styles";
+
+type FormT = {
+  from: string;
+  to: string;
+  amount: number;
+  isSigned: boolean;
+};
+
+const defaultForm: FormT = {
+  from: "",
+  to: "",
+  amount: 0,
+  isSigned: false,
+};
+
+const TransactionModal = (): ReactElement => {
+  const { network, account, balance, setBalance } = useGlobalState();
+  const [form, setForm] = useState<FormT>(defaultForm);
+  const [sending, setSending] = useState<boolean>(false);
+  const [transactionSig, setTransactionSig] = useState<string>("");
+
+  const onFieldChange = (field: string, value: string) => {
+    if (field === "amount" && !!value.match(/\D+/)) {
+      console.log(value);
+      return;
+    }
+
+    setForm({
+      ...form,
+      [field]: value,
+    });
+  };
+
+  // *Step 5*: implement a function that transfer funds
+  const transfer = async () => {
+    if (!account) return;
+
+    try {
+      setTransactionSig("");
+
+      const connection = new Connection(clusterApiUrl(network), "confirmed");
+
+      const instructions = SystemProgram.transfer({
+        fromPubkey: account.publicKey,
+        toPubkey: new PublicKey(form.to),
+        lamports: form.amount,
+      });
+
+      const transaction = new Transaction().add(instructions);
+
+      const signers = [
+        {
+          publicKey: account.publicKey,
+          secretKey: account.secretKey,
+        },
+      ];
+
+      setSending(true);
+      const confirmation = await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        signers
+      );
+      setTransactionSig(confirmation);
+      setSending(false);
+
+      const updatedBalance = await refreshBalance(network!, account);
+      setBalance(updatedBalance!);
+      message.success(`Transaction confirmed`);
+    } catch (error) {
+      message.error(
+        "Transaction failed, please check your inputs and try again"
+      );
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <CheckContainer>
+        <CheckImage src="/check.jpeg" alt="Check" />
+        <CheckFrom>{`FROM: ${account?.publicKey}`}</CheckFrom>
+
+        {transactionSig && (
+          <Processed
+            href={`https://explorer.solana.com/tx/${transactionSig}?cluster=devnet`}
+            target="_blank"
+          >
+            Processed - Review on Solana Block Explorer
+          </Processed>
+        )}
+
+        <CheckDate>
+          {new Date().toString().split(" ").slice(1, 4).join(" ")}
+        </CheckDate>
+        <RecipientInput
+          value={form.to}
+          onChange={(e) => onFieldChange("to", e.target.value)}
+        />
+        <AmountInput
+          value={form.amount}
+          onChange={(e) => onFieldChange("amount", e.target.value)}
+        />
+        <AmountText>
+          {form.amount <= 0 ? "" : converter.toWords(form.amount)}
+        </AmountText>
+        {sending ? (
+          <LoadingOutlined
+            style={{
+              fontSize: 24,
+              position: "absolute",
+              top: "69%",
+              left: "73%",
+            }}
+            spin
+          />
+        ) : (
+          <SignatureInput
+            onClick={transfer}
+            disabled={
+              !balance ||
+              form.amount / LAMPORTS_PER_SOL > balance ||
+              !form.to ||
+              form.amount == 0
+            }
+            type="primary"
+          >
+            Sign and Send
+          </SignatureInput>
+        )}
+        <RatioText>1 $SOL = 1,000,000,000 $L</RatioText>
+      </CheckContainer>
+    </>
+  );
+};
+
+export default TransactionModal;
---
+++ utils/index.ts
@@ -0,0 +1,42 @@
+// Import any additional classes and/or functions needed from Solana's web3.js library as you go along:
+import { Cluster, Keypair,Connection,clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { message } from "antd";
+
+
+// *Step 3*: implement a function that gets an account's balance
+const refreshBalance = async (network: Cluster, account: Keypair | null) => {
+  if (!account) return;
+
+  try {
+    const connection = new Connection(clusterApiUrl(network), "confirmed");
+    const publicKey = account.publicKey;
+    const balance = await connection.getBalance(publicKey);
+    return balance/LAMPORTS_PER_SOL;
+  } catch (error) {
+    console.log(error);
+    return 0;
+  }
+};
+
+
+// *Step 4*: implement a function that airdrops SOL into devnet account
+const handleAirdrop = async (network: Cluster, account: Keypair | null) => {
+  if (!account) return;
+
+  try {
+    const connection = new Connection(clusterApiUrl(network), "confirmed");
+    const publicKey = account.publicKey;
+    const confirmation = await connection.requestAirdrop(
+      publicKey,
+      LAMPORTS_PER_SOL
+    );
+    const result = await connection.confirmTransaction(confirmation, "confirmed");
+    return await refreshBalance(network, account);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+};
+
+
+export { refreshBalance, handleAirdrop };
